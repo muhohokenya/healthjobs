2a1fac43b9cb178c0ceb6cfe98e4e78be89075fd
Jeremy <muhohoweb@gmail.com>
2025-09-02 23:52:17 +0300

    Fix verbiage

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -263,7 +263,7 @@ const navigateToRegister = (): void => {
                         />
                         <div class="p-6">
                             <h3 class="text-xl font-semibold text-gray-900 mb-2">Compassion and Integrity</h3>
-                            <p class="text-gray-600">elationships that build trust and healing.</p>
+                            <p class="text-gray-600">Relationships that build trust and healing.</p>
                         </div>
                     </div>
                 </div>
