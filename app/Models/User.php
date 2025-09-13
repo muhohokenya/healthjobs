d5bd76ffcb9a0498d5867941f449c4ac4ebdbcdc
Jeremy <muhohoweb@gmail.com>
2025-09-14 02:34:51 +0300

    Fix Storage

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
@@ -41,13 +42,13 @@ class User extends Authenticatable
      * Get the user's first name.
      */
     // Option 2: Return empty string when null
+    /**
+     * Get the user's avatar URL.
+     */
     protected function avatar(): Attribute
     {
-        $domain = env('APP_DOMAIN');
         return Attribute::make(
-            get: fn (?string $value) => $value
-                ? $domain.$value
-                : '',
+            get: fn (?string $value) => $value ? Storage::url($value) : null
         );
     }
 
